2924aac45faee4e8bea8298d577e00bce4efc48e
DimaTheCat <skr1ng4m1ng@gmail.com>
2020-09-08 12:11:43 -0500

    Add dificulty and randomaser

---
+++ Game.py
@@ -0,0 +1,10 @@
+#RobcoOS v1.0.2 v2.5.1 v3.0.2 v4.3.0 V5.5.1
+#Password Required
+
+#Attempts Remaining: * * * *
+from random import randints
+
+Dificulty = ['v1.0.2', 'v2.5.1', 'v3.0.2', 'v4.3.0', 'V5.5.1'] #Very easy, easy, normal, hard, very hard
+
+GameDif = Dificulty[randint(0, 4)]
+
