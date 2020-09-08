78117b1023a1c06e83ce0ebe97fad11912777c00
DimaTheCat <skr1ng4m1ng@gmail.com>
2020-09-08 12:32:51 -0500

    Add Attempts count

--- Game.py
+++ Game.py
@@ -6,10 +6,14 @@ Dificulty = ['v1.0.2', 'v2.5.1', 'v3.0.2', 'v4.3.0', 'V5.5.1'] #Very easy, easy,
 
 GameDif = Dificulty[randint(0, 4)]
 
+Attempts = 4
+
 Greting = """RobcoOS {0}
-Password Required\n""".format(GameDif)
+Password Required\n\n""".format(GameDif)
 
 #Prints a greeting
 for char in Greting:
     print(char, end='', flush=True)
     sleep(0.05)
+
+print("Attempts Remaining:{}".format(' *'*Attempts))
