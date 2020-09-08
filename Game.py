df9943b3b2790c766b9ea163607d9ff0e9c48084
DimaTheCat <skr1ng4m1ng@gmail.com>
2020-09-08 13:18:29 -0500

    Add Game and WordCheck functions

--- Game.py
+++ Game.py
@@ -2,18 +2,23 @@
 from random import randint
 from time import sleep
 
-Dificulty = ['v1.0.2', 'v2.5.1', 'v3.0.2', 'v4.3.0', 'V5.5.1'] #Very easy, easy, normal, hard, very hard
+Dificulty = ['v1.0.2', 'v2.5.1', 'v3.0.2', 'v4.3.0', 'v5.5.1'] #Very easy, easy, normal, hard, very hard
 
 GameDif = Dificulty[randint(0, 4)]
 
-Attempts = 4
-
 Greting = """RobcoOS {0}
 Password Required\n\n""".format(GameDif)
+
+#Check the word
+def WordCheck(word, password):
+    pass
 
 #Prints a greeting
 for char in Greting:
     print(char, end='', flush=True)
     sleep(0.05)
 
-print("Attempts Remaining:{}".format(' *'*Attempts))
+def Game():
+    Attempts = 4
+    print("Attempts Remaining:{}".format(' *'*Attempts))
+    
