33db417dcf67b1442a9d31c54126bc7c7c83f7f0
DimaTheCat <skr1ng4m1ng@gmail.com>
2020-09-08 12:29:05 -0500

    Add greating

--- Game.py
+++ Game.py
@@ -1,10 +1,15 @@
-#RobcoOS v1.0.2 v2.5.1 v3.0.2 v4.3.0 V5.5.1
-#Password Required
-
 #Attempts Remaining: * * * *
 from random import randint
+from time import sleep
 
 Dificulty = ['v1.0.2', 'v2.5.1', 'v3.0.2', 'v4.3.0', 'V5.5.1'] #Very easy, easy, normal, hard, very hard
 
 GameDif = Dificulty[randint(0, 4)]
 
+Greting = """RobcoOS {0}
+Password Required\n""".format(GameDif)
+
+#Prints a greeting
+for char in Greting:
+    print(char, end='', flush=True)
+    sleep(0.05)
